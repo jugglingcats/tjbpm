3b6c49f3fc47c3f0fe3f28a9c4011616b2e63a15
Alfie Kirkpatrick <github@akirkpatrick.com>
2013-06-07 18:07:30 +0100

    basic working example

--- readme.txt
+++ readme.txt
@@ -1,2 +1,16 @@
 Look at DefaultWorkItemManager
 - Shows how work items are completed by signalling
+
+
+Stories
+
+- Create simple workflow using gateways
+
+- Show how user can complete a task
+
+- Add parameter to task completion and mapping to variable
+
+- Select specific outgoing connection according to variable
+
+
+
--- src/test/java/BasicTests.java
+++ src/test/java/BasicTests.java
@@ -1,18 +1,17 @@
 import org.drools.KnowledgeBase;
 import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderConfiguration;
 import org.drools.builder.KnowledgeBuilderFactory;
 import org.drools.builder.ResourceType;
 import org.drools.event.DefaultProcessEventListener;
-import org.drools.event.process.ProcessEventListener;
 import org.drools.event.process.ProcessNodeTriggeredEvent;
 import org.drools.io.ResourceFactory;
-import org.drools.process.instance.WorkItemHandler;
 import org.drools.runtime.StatefulKnowledgeSession;
 import org.drools.runtime.process.*;
-import org.jbpm.ruleflow.instance.RuleFlowProcessInstance;
 import org.junit.Test;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -21,43 +20,48 @@ import java.util.List;
  * Time: 22:09
  * To change this template use File | Settings | File Templates.
  */
-public class BasicTests extends DefaultProcessEventListener {
+public class BasicTests extends DefaultProcessEventListener implements org.drools.runtime.process.WorkItemHandler {
 
     @Test
     public void test() {
-        KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+        KnowledgeBuilderConfiguration kbc = KnowledgeBuilderFactory.newKnowledgeBuilderConfiguration();
+        KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder(kbc);
+
         kbuilder.add(ResourceFactory.newClassPathResource("test.bpmn"), ResourceType.BPMN2);
         KnowledgeBase kbase = kbuilder.newKnowledgeBase();
 
         StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
 
-//        ksession.getWorkItemManager().registerWorkItemHandler("Human Task", new WSHumanTaskHandler());
-        ksession.getWorkItemManager().registerWorkItemHandler("Manual Task", new WorkItemHandler() {
-            @Override
-            public void executeWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
-                System.out.println("Work item name: "+workItem.getName()+", id: "+workItem.getId());
-            }
-
-            @Override
-            public void abortWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
-            }
-        });
-
+        ksession.getWorkItemManager().registerWorkItemHandler("Manual Task", this);
         ksession.addEventListener(this);
 
         ProcessInstance processInstance = ksession.startProcess("test");
-        ksession.getWorkItemManager().completeWorkItem(1, null);
-        NodeInstanceContainer container= (NodeInstanceContainer) processInstance;
-        for ( NodeInstance s : container.getNodeInstances() ) {
-            System.out.println("Active node: "+s.getNodeName()+", id:" +s.getNodeId());
+
+        System.out.println("Completing work item with id: 1");
+        Map<String, Object> results = new HashMap<String, Object>();
+        results.put("transition", "reject");
+        ksession.getWorkItemManager().completeWorkItem(1, results);
+
+        // output where we ended up
+        NodeInstanceContainer container = (NodeInstanceContainer) processInstance;
+        for (NodeInstance s : container.getNodeInstances()) {
+            System.out.println("Active node: " + s.getNodeName() + ", id:" + s.getNodeId());
         }
 
-
-        System.out.println("Process state: "+processInstance.getState());
+        System.out.println("Process state: " + processInstance.getState());
     }
 
     @Override
     public void beforeNodeTriggered(ProcessNodeTriggeredEvent event) {
-        System.out.println("Before node: "+event.getNodeInstance().getNodeName());
+        System.out.println("Before node: " + event.getNodeInstance().getNodeName());
+    }
+
+    @Override
+    public void executeWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
+        System.out.println("Work item name: " + workItem.getName() + ", id: " + workItem.getId());
+    }
+
+    @Override
+    public void abortWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
     }
 }
